00ca4f5f8b1f249a5900bcb02e98ed6c6b7c9b25
Camille Supery <csupery@gmail.com>
2020-10-16 20:36:30 +0200

    Added scaffold Partner

---
+++ db/migrate/20201016183547_create_partners.rb
@@ -0,0 +1,12 @@
+class CreatePartners < ActiveRecord::Migration[6.0]
+  def change
+    create_table :partners do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
