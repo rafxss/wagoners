51e9eb63d640a7a8a13d41fba1fd95a060cf0269
Camille Supery <csupery@gmail.com>
2020-10-16 22:46:41 +0200

    Changes for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :users_to_offers, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
+  validates :address, presence: true
 
   enum category: [:bronce, :silver, :gold]
 
