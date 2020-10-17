04474391e550ef5a56f502936e2d930e5b0ac0fb
Haess Romani <hromaza@gmail.com>
2020-10-17 08:34:56 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_10_17_112653) do
 
   # These are extensions that must be enabled in order to support this database
@@ -67,10 +66,6 @@ ActiveRecord::Schema.define(version: 2020_10_17_112653) do
     t.float "longitude"
     t.integer "category"
     t.date "date_of_birth"
-    t.string "provider"
-    t.string "uid"
-    t.string "token"
-    t.datetime "token_expiry"
     t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
