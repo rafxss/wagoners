2722f7a7a3e2f96f4df84d3ae27f885b7b93402e
Rafa Ruigomez <rafaruigomez@gmail.com>
2020-10-17 12:38:01 +0200

    added level(bronce, silver...) to offer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_024316) do
+ActiveRecord::Schema.define(version: 2020_10_17_101849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_10_17_024316) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "due_date"
     t.string "url"
+    t.string "level"
     t.index ["partner_id"], name: "index_offers_on_partner_id"
   end
 
