32f357977f4d0f7db3cb480db8c673ca488139be
Camille Supery <csupery@gmail.com>
2020-10-17 02:11:04 +0200

    adding due date deleting offer over_due

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -9,4 +9,9 @@ class Offer < ApplicationRecord
   validates :voucher, presence: true
   validates :category, presence: true
 
+  def due_date_changes
+    if (Date.today > self.due_date)
+      self.destroy
+    end
+  end
 end
