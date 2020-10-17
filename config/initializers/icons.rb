08dd7b41fa7af3ebd82ac8d867e142c0068d1c95
Camille Supery <csupery@gmail.com>
2020-10-17 18:02:44 +0200

    Added the icons

---
+++ config/initializers/icons.rb
@@ -0,0 +1,9 @@
+OFFER_ICONS = {
+  con_credito: 'fas fa-hand-holding-usd',
+  sin_credito: 'fab fa-creative-commons-nc-eu',
+  automoviles: 'fas fa-car',
+  dirigidas: 'fas fa-user-tag',
+  no_clientes: 'fas fa-users-slash',
+  puntos: 'fas fa-award',
+  comercial: 'fas fa-tag'
+}
